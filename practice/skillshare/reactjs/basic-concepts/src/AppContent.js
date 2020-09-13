86721383e47485c0f9c6a9c895dbf9e7740b1a50
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-13 17:35:36 +0500

    AppContent.js

---
+++ practice/skillshare/reactjs/basic-concepts/src/AppContent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AppContent extends Component {
+    render() {
+        return(
+            <div className="app-content">
+                <h3>React is created by Facebook in 2011</h3>
+            </div>
+        );
+    }
+}
--- practice/skillshare/reactjs/basic-concepts/src/index.js
+++ practice/skillshare/reactjs/basic-concepts/src/index.js
@@ -2,15 +2,18 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
+import AppContent from './AppContent';
 
 
 class App extends Component {
   render() {
-    return <div>
+    return(
+    <div>
       <AppHeader/>
-      <h3>Created by Facebook's Software Engineer in 2011</h3>
+      <AppContent/>
       <AppFooter/>
-    </div>;
+    </div>
+    );
   }
 }
 
