55b440b0d71ae86308d2d68f5025e9bb53fdca5b
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-13 17:23:18 +0500

    AppHeader.js

---
+++ practice/skillshare/reactjs/basic-concepts/src/AppHeader.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+
+
+export default class AppHeader extends Component {
+    render() {
+        return ( 
+        <Fragment>
+            <h1>Hello World, from Danish Ul Hassan</h1>
+            <strong>Powered by ReactJS</strong>
+            <hr/>
+        </Fragment>
+        );
+    }
+}
--- practice/skillshare/reactjs/basic-concepts/src/index.js
+++ practice/skillshare/reactjs/basic-concepts/src/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './AppHeader';
 
 
 class App extends Component {
   render() {
     return <div>
-      <h1>Hello World, from Danish Ul Hassan</h1>
-      <strong>Powered by ReactJS</strong>
-      <hr/>
+      <AppHeader/>
+      <h3>Created by Facebook's Software Engineer in 2011</h3>
     </div>;
   }
 }
