329f86b890503b690933decf903853a5ff008aba
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-13 17:50:40 +0500

    Adding AppContent.css

--- practice/skillshare/reactjs/basic-concepts/src/AppContent.js
+++ practice/skillshare/reactjs/basic-concepts/src/AppContent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './AppContent.css';
 
 export default class AppContent extends Component {
     render() {
-        return(
-            <div className="app-content">
-                <h3>React is created by Facebook in 2011</h3>
-            </div>
+        return ( 
+        <div className = "app-content">
+            <h3> React is created by Facebook in 2011 </h3>
+             </div >
         );
     }
 }
