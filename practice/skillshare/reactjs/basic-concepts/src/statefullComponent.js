7f7e5f55b46b0de1ea8acbe9275886d51c3a09ef
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-14 09:17:11 +0500

    Creating Increment and Decrement Functions

--- practice/skillshare/reactjs/basic-concepts/src/statefullComponent.js
+++ practice/skillshare/reactjs/basic-concepts/src/statefullComponent.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 
 export default class StatefullComponent extends Component {
+    state = {
+        num:0
+    };
+
+    increment = () => {
+        this.setState({num: this.state.num +1});
+    }
+    decrement = () => {
+        this.setState({num: this.state.num -1});
+    }
     render() {
         return(
             <div className="container">
-                <h1 className="bg-info">
+                <h4 className="alert alert-info">
                 React JS Counter Application Using StatefullComponent
-                </h1>
+                </h4>
+                <h5>The Value of Num is: {this.state.num}</h5>
+                <button className="btn btn-primary" onClick={this.increment}>
+                    Increment
+                </button>
+                <button className="ml-2 btn btn-primary" onClick={this.decrement}>
+                    Decrement
+                </button>
             </div>
         );
     }
