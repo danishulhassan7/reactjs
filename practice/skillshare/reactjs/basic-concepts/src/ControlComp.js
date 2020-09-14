8d4a38650dc8816b97bf9573fab931274391ef9a
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-14 18:42:07 +0500

    Working on ControlComp 2

--- practice/skillshare/reactjs/basic-concepts/src/ControlComp.js
+++ practice/skillshare/reactjs/basic-concepts/src/ControlComp.js
@@ -3,23 +3,27 @@ import './UncontrolComp.css';
 
 export default class ControlComp extends Component {
     state = {
-        name = '',
-        email = '',
-        phone ='',
-        picture =''
+        name:'',
+        email:'',
+        phone:'',
+        picture:''
+    }
+
+    tfHandler = (evt) => {
+        console.log(evt.target);
     }
     render() {
         return(
             <div className="container mt-3">
                 <h1 className="my-2 alert alert-info">Controlled Components Demo</h1>
                 <h2>Add a new contact</h2>
-                <form className="form" onSubmit={}>
+                <form className="form">
                     <div className="form-group-row">
                         <label htmlFor="name" className="control-label col-md-4">
                             Name: 
                         </label>
                         <div className="col-md-6">
-                            <input type="text" name="name" className="form-control"/>
+                            <input type="text" onChange={this.tfHandler} name="name" className="form-control"/>
                         </div>
                     </div>
                     <div className="form-group-row">
