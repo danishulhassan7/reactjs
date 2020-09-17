eb05d6b2d83195ae18f54363f44c084c486e1c71
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-17 20:01:37 +0500

    Working on ContactList.js file

--- practice/skillshare/reactjs/basic-concepts/src/ContactList.js
+++ practice/skillshare/reactjs/basic-concepts/src/ContactList.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import ContactCard from './Arrays';
+
+
+const ContactList = ({contacts}) => (
+    <div>
+       { contacts.map(c => <ContactCard contact={c} key={c.id} />) }
+    </div>
+);
+
+export default ContactList;
