093b6dd1e2eb7807f13fb61dd126bcf0cb37c12f
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-17 11:24:49 +0500

    Working with arrays

--- practice/skillshare/reactjs/basic-concepts/src/index.js
+++ practice/skillshare/reactjs/basic-concepts/src/index.js
@@ -17,7 +17,7 @@ import ContactCard from './Arrays';
 class App extends Component {
   render() {
 
-
+// Working With Arrays
     const contact = {
       "id": 1,
       "name": "Alysia D'Hooge",
@@ -26,6 +26,44 @@ class App extends Component {
       "phone": "1571022046",
       "picture": "http://kvinod.com/old_ci/randomdata/images/women/1.jpg"
   };
+
+  const contacts = [
+    {
+        "id": 2,
+        "name": "Ruby Tothe",
+        "gender": "Male",
+        "email": "rtothe1@usa.gov",
+        "phone": "1983800201",
+        "picture": "http://kvinod.com/old_ci/randomdata/images/men/2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Reinald Tace",
+        "gender": "Male",
+        "email": "rtace2@stanford.edu",
+        "phone": "7704172788",
+        "picture": "http://kvinod.com/old_ci/randomdata/images/men/3.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Phoebe Kingsnod",
+        "gender": "Female",
+        "email": "pkingsnod3@yellowbook.com",
+        "phone": "8137639930",
+        "picture": "http://kvinod.com/old_ci/randomdata/images/women/4.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Sharai Manach",
+        "gender": "Female",
+        "email": "smanach4@scientificamerican.com",
+        "phone": "9459000368",
+        "picture": "http://kvinod.com/old_ci/randomdata/images/women/5.jpg"
+    }
+];
+
+// Working With Arrays Ends 
+
     const footerProps = {
       website: 'https://danishulhassan7portfolio.web.app',
       company: 'XPF Inc.',
@@ -35,6 +73,7 @@ class App extends Component {
     <div className="app container">
       <AppHeader title="AddressBook App version 1.0.8" subheading="Created by XPF"/>
       <AppContent/>
+      <h1 className="alert alert-info">Working with arrays</h1>
       <ContactCard contact={contact} />
       <ControlComp/>
       <UncontrolComp/>
