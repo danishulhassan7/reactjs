81b78b33fd576e8a8008b672e5968530487663cd
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-24 21:28:16 +0500

    Lifting up the dishes.js in main file App.js

--- practice/coursera/react/confusion/src/App.js
+++ practice/coursera/react/confusion/src/App.js
@@ -2,9 +2,18 @@ import React, { Component }from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render(){
     return(
       <div className="App">
@@ -13,7 +22,7 @@ class App extends Component {
             <NavbarBrand href="/"><h3>XPF Delicious Fooderia</h3></NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes = {this.state.dishes} />
       </div>
     );
   }
