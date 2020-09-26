2493ad8b8ffb81fba89ee0c4c458c8cc04de1c25
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-26 22:32:28 +0500

    seperating menudetails component (asg-1)

--- practice/coursera/react/confusion/src/components/DetailedMenuComp.js
+++ practice/coursera/react/confusion/src/components/DetailedMenuComp.js
@@ -1,49 +1,49 @@
-// import React, { Component } from 'react';
-// import { Card, CardBody, CardImg, CardTitle, CardText } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardBody, CardImg, CardTitle, CardText } from 'reactstrap';
 
 
-// class DetailedMenuComp extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             selectedDish: null
-//         };
-//     }
+class DetailedMenuComp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDish: null
+        };
+    }
 
-//     onDishSelect(dish) {
-//         this.setState({ selectedDish: dish });
-//     }
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    }
 
-//     renderDish(dish) {
-//         if (dish != null)
-//             return (
-//                 <div className="col-md-8 offset-md-2 mt-3">
-//                     <Card>
-//                         <CardImg top className="img-thumbnail" src={dish.image} alt={dish.name} />
-//                         <CardBody>
-//                             <CardTitle> {dish.name} </CardTitle>
-//                             <CardText> {dish.description} </CardText>
-//                         </CardBody>
-//                     </Card>
-//                 </div>
-//             );
-//         else
-//             return (
-//                 <div></div>
-//             );
-//     }
+    renderDish(dish) {
+        if (dish != null)
+            return (
+                <div className="col-md-8 offset-md-2 mt-3">
+                    <Card>
+                        <CardImg top className="img-thumbnail" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name} </CardTitle>
+                            <CardText> {dish.description} </CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
 
 
-//     render() {
-//         const detailedMenu = this.props.dishes;
-//         return (
-//             <div className="container">
-//                 <div className="row">
-//                 { this.renderDish(this.state.selectedDish) }
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
+    render() {
+        const detailedMenu = this.props.dishes;
+        return (
+            <div className="container">
+                <div className="row">
+                { this.renderDish(this.state.selectedDish) }
+                </div>
+            </div>
+        );
+    }
+}
 
-// export default DetailedMenuComp;
+export default DetailedMenuComp;
