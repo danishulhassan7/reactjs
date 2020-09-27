565e1f2a3d8747529d3ea627ff9f2cc46e4e9618
danishulhassan7 <danishulhassan7@gmail.com>
2020-09-27 22:44:54 +0500

    Following the documentation of asg-1

---
+++ practice/coursera/react/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+
+
+class DishdetailComponent extends Component {
+    render() {
+        return(
+
+        );
+    }
+}
+
+export default DishdetailComponent;
