8fcd694cb553aa857c3f2009e76d2a9de7848f31
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-06 18:21:31 +0500

    BrowserRouter and Route

--- practice/youtube/hooks-routers/src/App.js
+++ practice/youtube/hooks-routers/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+      </div>
+    </BrowserRouter>
   );
 }
 
