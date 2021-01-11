e0b3cd9b44d201f07e2e3db20514ee8e784371fc
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-12 00:27:45 +0500

    React simple app: Almost All Done

--- practice/youtube/react-bootstrap/simple-template/src/App.js
+++ practice/youtube/react-bootstrap/simple-template/src/App.js
@@ -6,25 +6,25 @@ import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { NoMatch } from './components/NoMatch';
 import { Layout } from './components/Layout';
-import { NavigationBar }  from './components/NavigationBar';
+import { NavigationBar } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <NavigationBar />
-        <Jumbotron />
-        <Layout>
-          <Router>
+        <Router>
+          <NavigationBar />
+          <Jumbotron />
+          <Layout>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route component={NoMatch} />
             </Switch>
-          </Router>
-        </Layout>
+          </Layout>
+        </Router>
       </React.Fragment>
     );
   }
