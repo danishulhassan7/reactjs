15ae457c4467789160ebdb600611782f1f9ccbb6
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-11 23:01:36 +0500

    React simple app: Created layout comp

--- practice/youtube/react-bootstrap/simple-template/src/App.js
+++ practice/youtube/react-bootstrap/simple-template/src/App.js
@@ -1,25 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import { Form } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {Home} from './components/Home';
-import {About} from './components/About';
-import {Contact} from './components/Contact';
-import {NoMatch} from './components/NoMatch';
-
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Contact } from './components/Contact';
+import { NoMatch } from './components/NoMatch';
+import { Layout } from './components/Layout';
 
 
 class App extends Component {
   render() {
-    return(
+    return (
       <React.Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
       </React.Fragment>
     );
   }
