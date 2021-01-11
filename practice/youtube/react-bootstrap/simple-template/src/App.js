73e5830b2ec1821ce45effe8a73f231a1166612a
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-11 23:53:05 +0500

    React simple app: Created Nav Comp

--- practice/youtube/react-bootstrap/simple-template/src/App.js
+++ practice/youtube/react-bootstrap/simple-template/src/App.js
@@ -6,12 +6,13 @@ import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { NoMatch } from './components/NoMatch';
 import { Layout } from './components/Layout';
-
+import { NavigationBar }  from './components/NavigationBar';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavigationBar />
         <Layout>
           <Router>
             <Switch>
