47d8c3ac15108994568d9b4ef349d152191e1ba4
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-12 14:57:28 +0500

    React Bootstrap: Bootstrap Comp.

--- practice/youtube/react-bootstrap/react-bootstrap-prac/src/App.js
+++ practice/youtube/react-bootstrap/react-bootstrap-prac/src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Breadcrumb, Alert, Button, Container, Row, Col, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Form>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="example@yahoo.com" />
+            <Form.Text className="text-muted">
+              We don't share your email with anyone.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="Password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">Login</Button>
+        </Form>
+
+        <Card className="my-3" style={{ color: "#000" }}>
+          <Card.Img src="https://picsum.photos/200/80" />
+          <Card.Body>
+            <Card.Title>Hello World</Card.Title>
+            <Card.Text> The way you find the world and expore the world is quite different. </Card.Text>
+            <Button variant="warning">Read More >> </Button>
+          </Card.Body>
+        </Card>
+
+        <Breadcrumb>
+          <Breadcrumb.Item> Home </Breadcrumb.Item>
+          <Breadcrumb.Item> Services </Breadcrumb.Item>
+          <Breadcrumb.Item> About </Breadcrumb.Item>
+          <Breadcrumb.Item active> Contact </Breadcrumb.Item>
+        </Breadcrumb>
+
+        <Alert variant="danger"> Something went wrong!!! </Alert>
       </header>
     </div>
   );
