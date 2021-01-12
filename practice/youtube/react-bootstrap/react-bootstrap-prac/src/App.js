233db428c8b4e50abe817927ae4a3b456c2e0bc4
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-12 15:11:02 +0500

    React Bootstrap: Done with Basic Comp

--- practice/youtube/react-bootstrap/react-bootstrap-prac/src/App.js
+++ practice/youtube/react-bootstrap/react-bootstrap-prac/src/App.js
@@ -6,42 +6,57 @@ import { Card, Breadcrumb, Alert, Button, Container, Row, Col, Form } from 'reac
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <Container>
+        <header className="App-header">
+        <Row>
         <Form>
+          <Col md>
           <Form.Group controlId="formEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="example@yahoo.com" />
-            <Form.Text className="text-muted">
-              We don't share your email with anyone.
-            </Form.Text>
-          </Form.Group>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="example@yahoo.com" />
+              <Form.Text className="text-muted">
+                We don't share your email with anyone.
+              </Form.Text>
+            </Form.Group>
+          </Col>
 
+          <Col md>
           <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="Password" placeholder="Password" />
-          </Form.Group>
-          <Button variant="primary" type="submit">Login</Button>
-        </Form>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="Password" placeholder="Password" />
+            </Form.Group>
+            </Col>
+            <Button variant="primary" type="submit">Login</Button>
+          </Form>
+        </Row>
 
-        <Card className="my-3" style={{ color: "#000" }}>
-          <Card.Img src="https://picsum.photos/200/80" />
-          <Card.Body>
-            <Card.Title>Hello World</Card.Title>
-            <Card.Text> The way you find the world and expore the world is quite different. </Card.Text>
-            <Button variant="warning">Read More >> </Button>
-          </Card.Body>
+
+        <Row>
+          <Col>
+          <Card className="my-3" style={{ color: "#000" }}>
+            <Card.Img src="https://picsum.photos/200/80" />
+            <Card.Body>
+              <Card.Title>Hello World</Card.Title>
+              <Card.Text> The way you find the world and expore the world is quite different. </Card.Text>
+              <Button variant="warning">Read More >> </Button>
+            </Card.Body>
         </Card>
+          </Col>
+        </Row>
 
-        <Breadcrumb>
-          <Breadcrumb.Item> Home </Breadcrumb.Item>
-          <Breadcrumb.Item> Services </Breadcrumb.Item>
-          <Breadcrumb.Item> About </Breadcrumb.Item>
-          <Breadcrumb.Item active> Contact </Breadcrumb.Item>
-        </Breadcrumb>
-
-        <Alert variant="danger"> Something went wrong!!! </Alert>
-      </header>
+        <Row>
+          <Col>
+          <Breadcrumb>
+            <Breadcrumb.Item> Home </Breadcrumb.Item>
+            <Breadcrumb.Item> Services </Breadcrumb.Item>
+            <Breadcrumb.Item> About </Breadcrumb.Item>
+            <Breadcrumb.Item active> Contact </Breadcrumb.Item>
+          </Breadcrumb>
+          <Alert variant="danger"> Something went wrong!!! </Alert>
+          </Col>
+        </Row>
+        </header>
+      </Container>
     </div>
   );
 }
